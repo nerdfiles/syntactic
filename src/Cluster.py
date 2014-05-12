e12fd8785831b963832edda256072c6009d225db
nerdfiles <nerdfiles@gmail.com>
2014-05-12 00:50:50 -0500

    Init port of Syntactic (Java).

---
+++ src/Cluster.py
@@ -0,0 +1,34 @@
+from sets import Set
+
+from syntax.learner import Vocabulary
+from syntax.learner import Word
+
+class Cluster:
+    self.id = null
+    self.totalSize = 0
+    self.wordCount = 0
+    self.words = Set(10) words
+    self.distribution = Null
+    self.vocab = Vocabulary
+    self.learner = Learner
+    self.isNew = false
+
+    def Cluster:
+
+    def clusterDistribution (self):
+        if learner.isClusterUpdated(self):
+            return distribution
+
+    def add(self, cluster):
+
+    def add(self, int item):
+
+    def remove(self, int item):
+
+    def reset(self):
+
+    def getID(self):
+
+    def isNew(self):
+
+    def setNew(self, bool b)
---
+++ src/ClusterContext.py
@@ -0,0 +1,5 @@
+from sytax.learner import corpus
+
+class ClusterContext(Context):
+    def ClusterContext (t1, t2):
+        super(t1, t2)
